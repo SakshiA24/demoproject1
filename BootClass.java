c387c4f7fdb3f1775e89d99c7ef2d7ba177cd5ed
SakshiA24 <103937827+SakshiA24@users.noreply.github.com>
2022-06-15 16:25:42 +0530

    Update BootClass.java

--- BootClass.java
+++ BootClass.java
@@ -2,13 +2,9 @@ package org.cap.demo;
 
 public class BootClass{
         public static void main(String args[]){
-             for(int i=`;i<10;i++)
-                System.out.println(i);
-              printMessage("Capgemini");
+             for(int i=1;i<10;i++)
+                System.out.println("CG");
+             
         }
-        
-        public static void printMessage(String msg){
-            for(int i=1;i<=msg.length();i++)
-                System.out.println(msg.substring(0,i));
-        }
+
 }
